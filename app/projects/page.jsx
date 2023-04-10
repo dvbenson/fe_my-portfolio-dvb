1f9c4926d3a9e9f1d36b2b1645506126164f4467
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-04-10 21:08:36 +0100

    project page cards complete, copy needed to do and state for buttons

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -8,8 +8,9 @@ export default function page() {
       Category: 'Backend',
       URL: 'https://badsauce-webservices.onrender.com/api',
       Repo: 'https://github.com/dvbenson/be_nc-news',
+      image: '/assets/be_nc-news.jpeg',
       Objectives: 'Create a node API to serve data to the frontend',
-      Technologies: 'Node, Express, MongoDB, Mongoose, Heroku, Render',
+      Stack: 'Node, Express, MongoDB, Mongoose, Heroku, Render',
       Learning: 'How to create a node API, how to deploy to Heroku and Render',
     },
     {
@@ -17,9 +18,20 @@ export default function page() {
       Category: 'Frontend',
       URL: 'https://badsauce-nc-news.netlify.app/',
       Repo: 'https://github.com/dvbenson/fe_nc-news',
+      image: '/assets/fe_nc-news.jpeg',
       Objectives: 'Create a React frontend to consume the node API',
-      Technologies: 'React, Axios, Netlify',
+      Stack: 'React, Axios, Netlify',
       Learning: 'How to create a React frontend, how to deploy to Netlify',
+    },
+    {
+      Title: 'Next.js Portfolio Project',
+      Category: 'Frontend',
+      URL: 'https://danielvb.dev/',
+      Repo: 'https://github.com/dvbenson/my-portfolio-dvb',
+      image: '/assets/dvb_portfolio.jpeg',
+      Objectives: 'Create a portfolio website using Next.js',
+      Stack: 'Next.js, Vercel',
+      Learning: 'How to create a Next.js website, how to deploy to Vercel',
     },
   ];
 
@@ -42,75 +54,95 @@ export default function page() {
             </div>
           </div>
         </section>
-        <section>
+        {/* <section>
           <div className='flex flex-row sm:flex-row items-center justify-center mt-4'>
             <button className={styles.filter_button}>Backend</button>
             <button className={styles.filter_button}>Frontend</button>
           </div>
-        </section>
+        </section> */}
         <section>
-          <div id='wrapper'>
-            <div
-              id='container'
-              className='flex flex-row justify-center items-center mt-4'
-            >
+          <div
+            id='wrapper'
+            className='flex flex-col justify-center items-center gap-10'
+          >
+            {projects.map((item) => (
               <div
-                id='card'
-                className='bg-[#336666] flex flex-col mx-8 lg:flex-row p-4 rounded-xl lg:w-[800px] lg:h-[320px] shadow-2xl'
+                id='container'
+                className='flex flex-row justify-center items-center mt-8'
               >
-                <div id='image-card' className='flex flex-col lg:mr-2'>
-                  <div
-                    id='project-title'
-                    className=' bg-[#969696] rounded-xl h-12 mb-1 text-center items-center justify-center flex'
-                  >
-                    <h1>Project Title</h1>
-                  </div>
-                  {/* <div
-                    id='site-image'
-                    className='w-auto min-[976px]:w-36 h-48 mt-1'
-                  > */}
-                  <img
-                    src='/assets/fe_nc-news.jpeg'
-                    alt='fe_nc-news'
-                    className='w-full h-auto'
-                  />
-                  {/* </div> */}
-                  <div
-                    id='link-buttons'
-                    className='flex flex-row justify-center items-center gap-2 mt-2'
-                  >
-                    <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-2 shadow-md'>
-                      Repo
-                    </button>
-                    <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-2 shadow-md'>
-                      Host
-                    </button>
-                  </div>
-                </div>
                 <div
-                  id='tabs-card'
-                  className='flex  flex-col bg-gray p-2 lg:ml-2 rounded-xl w-80 lg:w-[480px] h-auto '
+                  id='card'
+                  className='relative bg-[#336666] flex flex-col mx-8 min-[800px]:flex-row p-4 rounded-xl lg:w-[800px] lg:h-[320px] shadow-2xl'
                 >
-                  <div id='tab' className='bg-white p-2 rounded-xl h-80'>
-                    <p className='text-[#2C3333]'>Tab</p>
+                  <div
+                    id='image-card'
+                    className='flex flex-col min-[800px]:mr-2'
+                  >
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className='w-full h-auto lg:w-96 max-w-sm max-h-sm mt-4 rounded-xl shadow-md'
+                    />
+                    <div
+                      id='project-title'
+                      className='absolute -top-6 inset-x-0 left-1/2 min-[800px]:left-1/4 transform -translate-x-1/2 bg-gray rounded-full h-12 w-48 p-2 mb-1 text-center items-center justify-center flex'
+                    >
+                      <h1>{item.Title}</h1>
+                    </div>
+
+                    <div
+                      id='link-buttons'
+                      className='flex flex-row justify-center items-center gap-2 mt-2'
+                    >
+                      <a
+                        target='_blank'
+                        href={item.URL}
+                        rel='noopener noreferrer'
+                      >
+                        <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-3 shadow-md'>
+                          Host
+                        </button>
+                      </a>
+                      <a
+                        target='_blank'
+                        href={item.Repo}
+                        rel='noopener noreferrer'
+                      >
+                        <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-3 shadow-md'>
+                          Repo
+                        </button>
+                      </a>
+                    </div>
                   </div>
                   <div
-                    id='tabs-buttons'
-                    className='flex flex-row justify-center items-center gap-2'
+                    id='tabs-card'
+                    className='flex  flex-col bg-gray p-2 min-[800px]:ml-2 rounded-xl w-80 lg:w-[480px] h-auto w-auto max-h-80'
                   >
-                    <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
-                      Objectives
-                    </button>
-                    <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
-                      Stack
-                    </button>
-                    <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
-                      Learnings
-                    </button>
+                    <div id='tab' className='bg-white p-2 rounded-xl h-80'>
+                      <p className='text-[#2C3333]'>
+                        {item.Objectives}
+                        {item.Stack}
+                        {item.Learning}
+                      </p>
+                    </div>
+                    <div
+                      id='tabs-buttons'
+                      className='flex flex-row justify-center items-center gap-2'
+                    >
+                      <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
+                        Objectives
+                      </button>
+                      <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
+                        Stack
+                      </button>
+                      <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
+                        Learnings
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </section>
       </main>
