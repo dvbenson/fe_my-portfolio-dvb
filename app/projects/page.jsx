457f7bf28753232a9f9bc652fbadb1f52f02499e
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-04-11 09:42:31 +0100

    finish home page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,7 +1,11 @@
+'use client';
+import React, { useState } from 'react';
 import DefaultLayout from '@/components/DefaultLayout';
 import styles from '@/styles/projects.module.css';
 
 export default function page() {
+  const [tabDisplay, setTabDisplay] = useState('objectives');
+
   const projects = [
     {
       Title: 'node API',
@@ -54,95 +58,104 @@ export default function page() {
             </div>
           </div>
         </section>
-        {/* <section>
-          <div className='flex flex-row sm:flex-row items-center justify-center mt-4'>
-            <button className={styles.filter_button}>Backend</button>
-            <button className={styles.filter_button}>Frontend</button>
-          </div>
-        </section> */}
         <section>
           <div
             id='wrapper'
-            className='flex flex-col justify-center items-center gap-10'
+            className='flex flex-col justify-center items-center'
           >
-            {projects.map((item) => (
-              <div
-                id='container'
-                className='flex flex-row justify-center items-center mt-8'
-              >
+            <div className='mb-16 mt-8 flex flex-col justify-center items-center gap-10'>
+              {projects.map((item) => (
                 <div
-                  id='card'
-                  className='relative bg-[#336666] flex flex-col mx-8 min-[800px]:flex-row p-4 rounded-xl lg:w-[800px] lg:h-[320px] shadow-2xl'
+                  id='container'
+                  className='flex flex-row justify-center items-center mt-8 mb-8'
                 >
                   <div
-                    id='image-card'
-                    className='flex flex-col min-[800px]:mr-2'
+                    id='card'
+                    className='relative bg-[#336666] flex flex-col mx-8 min-[800px]:flex-row p-4 rounded-xl lg:w-[800px] lg:h-[320px] shadow-2xl'
                   >
-                    <img
-                      src={item.image}
-                      alt={item.title}
-                      className='w-full h-auto lg:w-96 max-w-sm max-h-sm mt-4 rounded-xl shadow-md'
-                    />
                     <div
-                      id='project-title'
-                      className='absolute -top-6 inset-x-0 left-1/2 min-[800px]:left-1/4 transform -translate-x-1/2 bg-gray rounded-full h-12 w-48 p-2 mb-1 text-center items-center justify-center flex'
+                      id='image-card'
+                      className='flex flex-col min-[800px]:mr-2'
                     >
-                      <h1>{item.Title}</h1>
-                    </div>
+                      <img
+                        src={item.image}
+                        alt={item.title}
+                        className='w-full h-auto lg:w-96 max-w-sm max-h-sm mt-4 rounded-xl shadow-md'
+                      />
+                      <div
+                        id='project-title'
+                        className='absolute -top-6 inset-x-0 left-1/2 min-[800px]:left-1/4 transform -translate-x-1/2 bg-gray rounded-full h-12 w-48 p-2 mb-1 text-center items-center justify-center flex'
+                      >
+                        <h1>{item.Title}</h1>
+                      </div>
 
-                    <div
-                      id='link-buttons'
-                      className='flex flex-row justify-center items-center gap-2 mt-2'
-                    >
-                      <a
-                        target='_blank'
-                        href={item.URL}
-                        rel='noopener noreferrer'
+                      <div
+                        id='link-buttons'
+                        className='flex flex-row justify-center items-center gap-2 mt-2'
                       >
-                        <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-3 shadow-md'>
-                          Host
-                        </button>
-                      </a>
-                      <a
-                        target='_blank'
-                        href={item.Repo}
-                        rel='noopener noreferrer'
-                      >
-                        <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-3 shadow-md'>
-                          Repo
-                        </button>
-                      </a>
-                    </div>
-                  </div>
-                  <div
-                    id='tabs-card'
-                    className='flex  flex-col bg-gray p-2 min-[800px]:ml-2 rounded-xl w-80 lg:w-[480px] h-auto w-auto max-h-80'
-                  >
-                    <div id='tab' className='bg-white p-2 rounded-xl h-80'>
-                      <p className='text-[#2C3333]'>
-                        {item.Objectives}
-                        {item.Stack}
-                        {item.Learning}
-                      </p>
+                        <a
+                          target='_blank'
+                          href={item.URL}
+                          rel='noopener noreferrer'
+                        >
+                          <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-3 shadow-md'>
+                            Host
+                          </button>
+                        </a>
+                        <a
+                          target='_blank'
+                          href={item.Repo}
+                          rel='noopener noreferrer'
+                        >
+                          <button className='rounded-xl bg-[#9BCFCF] p-2 grow mb-3 shadow-md'>
+                            Repo
+                          </button>
+                        </a>
+                      </div>
                     </div>
                     <div
-                      id='tabs-buttons'
-                      className='flex flex-row justify-center items-center gap-2'
+                      id='tabs-card'
+                      className='flex  flex-col bg-gray p-2 min-[800px]:ml-2 rounded-xl w-80 lg:w-[480px] h-auto w-auto max-h-80'
                     >
-                      <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
-                        Objectives
-                      </button>
-                      <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
-                        Stack
-                      </button>
-                      <button className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'>
-                        Learnings
-                      </button>
+                      <div id='tab' className='bg-white p-2 rounded-xl h-80'>
+                        <p className='text-[#2C3333]'>
+                          {tabDisplay === 'objectives'
+                            ? item.Objectives
+                            : tabDisplay === 'stack'
+                            ? item.Stack
+                            : tabDisplay === 'learning'
+                            ? item.Learning
+                            : item.Objectives}
+                        </p>
+                      </div>
+                      <div
+                        id='tabs-buttons'
+                        className='flex flex-row justify-center items-center gap-2'
+                      >
+                        <button
+                          onClick={() => setTabDisplay('objectives')}
+                          className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'
+                        >
+                          Objectives
+                        </button>
+                        <button
+                          onClick={() => setTabDisplay('stack')}
+                          className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'
+                        >
+                          Stack
+                        </button>
+                        <button
+                          onClick={() => setTabDisplay('learning')}
+                          className='rounded-xl bg-[#969696] p-2 mt-2 grow shadow-md'
+                        >
+                          Learnings
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </section>
       </main>
