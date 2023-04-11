51c6c19af6cb3cacb9493f8c7b3fc7f508bc0a58
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-04-11 20:04:44 +0100

    quick retake on about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,6 @@
 import DefaultLayout from '@/components/DefaultLayout';
 import styles from '@/styles/about.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function page() {
   const about_cards = {
@@ -81,55 +82,19 @@ export default function page() {
           </div>
         </section>
         <section>
-          <div className='container mx-auto sm:mx-42 px-4 md:px-0 mt-8'>
-            <div className='flex flex-col sm:flex-row flex-nowrap items-start justify-start md:space-x-16 h-auto sm:mx-42 sm:h-36 bg-white p-4 shadow-xl'>
-              <div className='mb-4 sm:mt-4 sm:ml-4 min-[976px]:w-6/12'>
-                <p className='text-left text-black sm:text-2xl lg:text-3xl'>
-                  <span className='text-2xl sm:text-4xl lg:text-5xl'>
-                    Dedicated
-                  </span>{' '}
-                  <span className='text-red'>problem-solver</span> with over a
-                  decade of professional experience, embracing change and
-                  focusing on <span className='text-red'>user-centered</span>{' '}
-                  design.
-                </p>
-              </div>
-              <div className='flex justify-center items-center flex-row space-x-2 mx-2 sm:mt-8 min-[490px]:mt-16 md:mt-6 min-[490px]:flex-col md:flex-row lg:mt-4 '>
-                <div className='flex justify-center items-center'>
-                  <ul className='text-3xl md:mt-6'>
-                    <li>Communicate.</li>
-                    <li>Collaborate.</li>
-                    <li>Create.</li>
-                  </ul>
-                </div>
-
-                <div className='relative'>
-                  <div className='flex items-center sm:mt-4 min-[490px]:mt-8'>
-                    <p className={styles.emoji}>👉</p>
-                    <button className={styles.cv_button}>
-                      View
-                      <br />
-                      CV
-                    </button>
-                  </div>
-                </div>
-              </div>
+          <div className='relative'>
+            <div className='flex items-center sm:mt-4 min-[490px]:mt-8'>
+              <p className={styles.emoji}>👉</p>
+              <button className={styles.cv_button}>
+                View
+                <br />
+                CV
+              </button>
             </div>
           </div>
         </section>
         <section>
           <div className='flex flex-col lg:flex-row justify-center items-center m-12 lg:m-16'>
-            <div className='md:flex md:flex-col md:justify-center md:items-center md:mx-8 min-[1048px]:my-36 min-[1340px]:my-0'>
-              <div id='bg-card_container' className={styles.bgcard_container}>
-                <div id='bg-card' className={styles.bgcard}>
-                  <h1 className='text-[#2C3333] text-center'>Big Bleeerb.</h1>
-                  <p className='text-[#2C3333] text-center'>
-                    Blerby Blurb. Yerb.
-                  </p>
-                </div>
-              </div>
-            </div>
-
             <div className='flex md:ml-4'>
               <ul className='gap-10 sm:flex sm:flex-row sm:flex-wrap justify-center items-center'>
                 {Object.entries(about_cards).map(([key, value]) => {
@@ -141,7 +106,11 @@ export default function page() {
                             <p className='text-3xl'>{key}.</p>
                           </div>
                           <div id='card-back' className={styles.back}>
-                            <p>{value}.</p>
+                            <ul>
+                              {value.map((value) => {
+                                return <li key={uuidv4()}>{value}</li>;
+                              })}
+                            </ul>
                           </div>
                         </div>
                       </div>
@@ -149,28 +118,6 @@ export default function page() {
                   );
                 })}
               </ul>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div className='container mx-auto sm:mx-42 px-4 md:px-0 mt-8'>
-            <div className='flex flex-col sm:flex-row flex-nowrap items-start justify-start h-auto sm:mx-42 sm:h-36 bg-pink p-4 lg:mx-42 shadow-xl'>
-              <div className='mb-4 sm:mt-4 sm:ml-4 md:w-6/12'>
-                <p className='text-black-gray sm:text-2xl lg:text-3xl'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  quis neque congue, rhoncus eros eget, dapibus est. Duis
-                  commodo, lectus nec faucibus laoreet, erat sapien tempus
-                  augue, a volutpat turpis risus vitae magna.
-                </p>
-              </div>
-              <div className='mb-4 sm:mt-4 sm:ml-4 md:w-6/12'>
-                <p className='text-black-gray sm:text-2xl lg:text-3xl'>
-                  {' '}
-                  Curabitur posuere est sit amet ex tincidunt, non viverra eros
-                  pretium. Mauris a elementum nisi. Fusce ut est pulvinar,
-                  placerat mi eget, pellentesque mauris.{' '}
-                </p>
-              </div>
             </div>
           </div>
         </section>
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import DefaultLayout from '@/components/DefaultLayout';
 import styles from '@/styles/projects.module.css';
