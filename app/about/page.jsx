b512012312f0885fe5dda3880cf25abe74af79a9
dvbenson <112098121+dvbenson@users.noreply.github.com>
2023-04-16 20:47:41 +0100

    about page complete

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -20,29 +20,29 @@ export default function page() {
           </div>
         </section>
         <section>
-          <div className='container mx-auto px-4 flex flex-col gap-x-20'>
-            <div className='container mx-auto px-4 w-full flex flex-col mt-8'>
+          <div className='container mx-auto px-4 flex flex-col'>
+            <div className='container mx-auto px-4 w-full flex flex-col mt-8 gap-5 md:gap-24'>
               <div
                 id='img-text block 1'
-                className='container mx-auto px-4 flex flex-col sm:flex-row justify-center items-center min-[480px]:mb-24'
+                className='container mx-auto px-4 flex flex-col md:flex-row justify-center items-center gap-x-20'
               >
-                <div className='flex justify-center items-center '>
+                <div className='flex justify-center items-center'>
                   <img
                     src='/assets/food_cartoon.svg'
                     alt='about'
-                    className='w-64 h-64 lg:w-96 lg:h-96 object-cover rounded-full'
+                    className='w-full h-full object-contain'
                   />
                 </div>
-                <div className='flex flex-col gap-4 min-[480px]:w-[320px] min-[480px]:h-[280px]'>
+                <div className='flex flex-col gap-4 min-[480px]:w-[400px] min-[480px]:h-[280px]'>
                   <h2 className='text-3xl lg:text-4xl xl:text-5xl'>
                     Get to know me.
                   </h2>
-                  <p className='font-serif font-medium text-base md:text-lg lg:text-2xl xl:text-xl'>
+                  <p className='font-serif font-medium text-base md:text-lg  xl:text-xl'>
                     I'm a full stack developer, I love the JavaScript eco-system
                     and I'm a highly intuitive self-teacher.
                   </p>
 
-                  <p className='font-serif font-medium text-base md:text-lg lg:text-2xl xl:text-xl'>
+                  <p className='font-serif font-medium text-base md:text-lg  xl:text-xl'>
                     Outside of programming you can find me drawing, designing,
                     baking or cooking. I'm an avid Linguist, having majored in
                     Languages, so I love learning a new phrase or two!
@@ -51,53 +51,53 @@ export default function page() {
               </div>
               <div
                 id='img-text block 2'
-                className='container mx-auto px-4 flex flex-col sm:flex-row justify-center items-center min-[480px]:mb-48'
+                className='container mx-auto px-4 flex flex-col md:flex-row justify-center items-center gap-x-20'
               >
-                <div className='flex flex-col  gap-4  max-[640px]:order-2 min-[480px]:w-[320px] min-[480px]:h-[280px] '>
+                <div className='flex justify-center items-center md:order-2'>
+                  <img
+                    src='/assets/coding_cartoon.svg'
+                    alt='about'
+                    className='w-full h-full object-contain '
+                  />
+                </div>
+                <div className='flex flex-col  gap-4 min-[480px]:w-[400px] min-[480px]:h-[280px] md:order-1'>
                   <h2 className='text-3xl lg:text-4xl xl:text-5xl'>
                     Experience.
                   </h2>
 
-                  <p className='font-serif font-medium text-base md:text-lg lg:text-2xl xl:text-xl'>
+                  <p className='font-serif font-medium text-base md:text-lg  xl:text-xl'>
                     With a background in user-centred services and products,
                     Agile is a methodology I am confident in and have used to
                     deliver successful products.
                   </p>
-                  <p className='font-serif font-medium text-base md:text-lg lg:text-2xl xl:text-xl'>
+                  <p className='font-serif font-medium text-base md:text-lg  xl:text-xl'>
                     Since then I have trained with Northcoders in full stack
                     development, working intensively to develop my skills and
                     hone in on what makes me tick as a developer.
                   </p>
                 </div>
-                <div className='flex justify-center items-center'>
-                  <img
-                    src='/assets/coding_cartoon.svg'
-                    alt='about'
-                    className='w-64 h-64 lg:w-96 lg:h-96 object-cover rounded-full  max-[640px]:order-1'
-                  />
-                </div>
               </div>
               <div
                 id='img-text block 3'
-                className='container mx-auto px-4 flex flex-col sm:flex-row justify-center items-center min-[480px]:mb-64'
+                className='container mx-auto px-4 flex flex-col md:flex-row justify-center items-center gap-x-20 md:mb-16 2xl:mb-24'
               >
-                <div className='flex justify-center items-center '>
+                <div className='flex justify-center items-center'>
                   <img
                     src='/assets/draw_cartoon.svg'
                     alt='about'
-                    className='w-64 h-64 lg:w-96 lg:h-96 object-cover rounded-full  m'
+                    className='w-full h-full object-contain'
                   />
                 </div>
-                <div className='flex flex-col  gap-4 min-[480px]:w-[320px] min-[480px]:h-[280px]'>
+                <div className='flex flex-col  gap-4 min-[480px]:w-[400px] min-[480px]:h-[280px]'>
                   <h2 className='text-3xl lg:text-4xl xl:text-5xl'>Skills.</h2>
 
-                  <p className='font-serif font-medium text-base md:text-lg lg:text-2xl xl:text-xl'>
+                  <p className='font-serif font-medium text-base md:text-lg  xl:text-xl'>
                     I am able to take a product through its entire lifecycle.
                     Whether that's confidently building a backend REST API in
                     Node, routing with Express or creating a frontend UI in
                     React or Next.js.
                   </p>
-                  <p className='font-serif font-medium text-base md:text-lg lg:text-2xl xl:text-xl'>
+                  <p className='font-serif font-medium text-base md:text-lg  xl:text-xl'>
                     I have taken a particular interest in UI/UX design, becoming
                     knowledgeable in design practices and prototyping using
                     Figma and Framer, and styling with vanilla CSS or Tailwind
@@ -107,14 +107,21 @@ export default function page() {
               </div>
               <div
                 id='img-text block 4'
-                className='container mx-auto px-4 flex flex-col sm:flex-row justify-center items-center mt-16 sm:mt-0'
+                className='container mx-auto px-4 flex flex-col md:flex-row justify-center items-center mt-16 md:mt-0 gap-x-20 xl:gap-x-56'
               >
-                <div className='flex flex-col gap-4 max-[640px]:order-1 min-[480px]:w-[320px] min-[480px]:h-[280px]'>
+                <div className='flex justify-center items-center md:order-2'>
+                  <img
+                    src='/assets/error_cartoon.svg'
+                    alt='about'
+                    className='w-full h-full object-contain max-[640px]:mb-8'
+                  />
+                </div>
+                <div className='flex flex-col gap-4 min-[480px]:w-[400px] min-[480px]:h-[280px] md:order-1'>
                   <h2 className='text-3xl text-center md:-4xl lg:text-4xl xl:text-5xl'>
                     Stack.
                   </h2>
 
-                  <ul className='font-serif font-medium text-center text-base md:text-lg lg:text-2xl xl:text-xl'>
+                  <ul className='font-serif font-medium text-center text-base md:text-lg  xl:text-xl'>
                     <li> JavaScript, HTML, CSS</li>
                     <li> React, Next.js, Vue.js</li>
                     <li>React Native</li>
@@ -125,13 +132,6 @@ export default function page() {
                     <li> Figma, Framer</li>
                   </ul>
                 </div>
-                <div className='flex justify-center items-center'>
-                  <img
-                    src='/assets/error_cartoon.svg'
-                    alt='about'
-                    className='w-64 h-64 lg:w-96 lg:h-96 object-cover rounded-full  max-[640px]:order-2  max-[640px]:mb-8 '
-                  />
-                </div>
               </div>
             </div>
           </div>
@@ -139,19 +139,4 @@ export default function page() {
       </main>
     </DefaultLayout>
   );
-}
-
-{
-  /* <div className='flex justify-center items-center'>
-              <div className='relative'>
-                <div className='flex items-center'>
-                  <p className={styles.emoji}>👉</p>
-                  <button className={styles.cv_button}>
-                    View
-                    <br />
-                    CV
-                  </button>
-                </div>
-              </div>
-            </div> */
 }
